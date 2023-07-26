85872d72c67969973b049b6eb8d775c21a8f458e
obace <107771494+obace@users.noreply.github.com>
2023-07-27 00:06:11 +0800

    Update gua.sh

--- gua.sh
+++ gua.sh
@@ -1,1 +1,1 @@
-apt-get update && apt install curl -y  && curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash ./tm.sh -t R5Gd/5PCCp8YMgWKJTPnzm0reMHvKK4TaQx0T0fWpwg=
+apt-get update && apt install curl -y  && curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh && chmod +x tm.sh && bash ./tm.sh -t joISZRoJPT75zZiDejLlnNgEGhQbu0+NAGpp4oYm08k=
